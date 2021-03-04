1c3c99d0aa35f00fa1365cd23c89493a821e03dc
Zack Hammes <hamm4058@stthomas.edu>
2021-03-03 21:52:27 -0600

    Updated as of 3/3 9:52 PM

--- src/osshell.cpp
+++ src/osshell.cpp
@@ -12,27 +12,31 @@
 #include <stdio.h>
 #include <string>
 
+#include <sys/wait.h>
+#include <sys/mman.h>
+
 void freeArrayOfCharArrays(char **array, size_t array_length);
 void splitString(std::string text, char d, std::vector<std::string>& result);
 void vectorOfStringsToArrayOfCharArrays(std::vector<std::string>& list, char ***result);
 int getNumLinesInFile();
+void* createSharedMemory(size_t size);
 
 int main (int argc, char **argv)
 {
     //char *input;
     std::string input;
     char* charInput;
+    char* secondInput;
+    std::string secondInputString;
     char history[] = "history";
     char exiting[] = "exit";
+    char clear[] = "clear";
     int historySize = 0;
     char fileName[] = "history.txt";
     std::string currentLine;
 
     std::fstream file;
-    if(file){
-        std::cout << "B4 the open\n";
-    }
-    file.open(fileName);
+    file.open(fileName, std::fstream::in | std::fstream::out | std::fstream::app);
     //If the file does not exist
     if(!file){
         file.open(fileName, std::fstream::in | std::fstream::out | std::fstream::app);
@@ -41,16 +45,20 @@ int main (int argc, char **argv)
 
     //put everything currently in the file into historyList[]
     while(!file.eof()){
-        std::string line = "";
+        std::string line;
         getline(file, line);
-        //std::cout << line << "\n";
 
         //if first = \n dont add it
-        historyList[historySize] = line;
-        historySize++;
+        if(!line.empty()){
+            historyList[historySize] = line;
+            historySize++;
+        }
     }
+    file.close();
+    file.open(fileName, std::fstream::app);
 
     for(int j=0; j < historySize; j++){
+        printf("Printing what is currently in the history.txt file: ");
         std::cout<< historyList[j]<< std::endl;
     }
     
@@ -88,6 +96,10 @@ int main (int argc, char **argv)
     std::copy(string_os_path_list.begin(), string_os_path_list.end(), char_os_path_list);
     char_os_path_list[string_os_path_list.size()] = '\0'; // don't forget the terminating 0
 
+    for(int i = 0; i < 10; i++){
+        std::cout << os_path_list[i] << "\n";
+    }
+
     // Repeat:
     while(whileLoop == 1){
         //  Print prompt for user input: "osshell> " (no newline)
@@ -96,25 +108,27 @@ int main (int argc, char **argv)
         //  Get user input for next command
         std::getline(std::cin, input);
         if(input.empty()){
-            //printf("input was a new line character\n");
+            printf("input was a new line character\n");
             continue;
         }else{
-            //printf("got input\n");
+            
             charInput = new char[input.length()+1];
             std::strcpy(charInput, input.c_str());
+
             file << input << std::endl;
-            file.close();
-            file.open(fileName);
+
             std::vector<std::string> input_list;
 
             //std::string example_command = "ls -lh";
             char **input_list_exec;
-            vectorOfStringsToArrayOfCharArrays(input_list, &input_list_exec);
+            //vectorOfStringsToArrayOfCharArrays(input_list, &input_list_exec);
             splitString(input, ' ', input_list);            
 
             historySize++;
             //Added input to the history list
             historyList[historySize] = input_list[0];
+            //printf("input_list[0] = ");
+            //std::cout << input_list[0] << std::endl;
             
             //Reassigning charInput to be the first element in the string that was just split
             std::strcpy(charInput, input_list[0].c_str());
@@ -123,55 +137,108 @@ int main (int argc, char **argv)
             if(strcmp(charInput, exiting) == 0){
                 size++;
                 exit(-1);
+            
             //  If command is `history` print previous N commands
             } else if(strcmp(charInput, history) == 0){
-                //std::string * historyList = new std::string[128];
-                std::string line = "";
-                int index = 0;
-                int numPrint = 0; 
+               // charInput only checks the first part of input
+                //std::string line = "";
+                //std::string inputlistAt1 = input_list[1];
+                //int index = 0;
+                //int numPrint = 0; 
+                //secondInput = new char[input.length()+1];
 
-                //numLinesInHist = getNumLinesInFile();
-                
-                /*
-                for(int i = 0; i < historySize; i++){
-                    getline(myFile, line);
-                    historyList[i] = line;
-                    std::cout << historyList[i];
+                for(int i = 1; i < historySize; i++){
+                        std::cout << "  " << i << ": " << historyList[i] << "\n";
                 }
-                */
 
-               
-                //check if input is histroy
-                if(input_list[1].empty()){
+                /*
+                printf("above line 144\n");
+                if(inputlistAt1.empty()){
                     //print all of the historyArray
-                    std::cout<< "os_path_list[1].empty() is true\n";
-                    for(int i = 0; i < historySize;i++){
+                    std::cout<< "input_list[1] is true\n";
+                    for(int i = 1; i < historySize; i++){
                         std::cout << "  " << i << ": " << historyList[i] << "\n";
                     }
+                }
+                secondInputString = input_list[1];
+                
+                std::strcpy(secondInput, secondInputString.c_str());
+
+                std::cout << "Second Input is: " << secondInput << std::endl;
+                
+               
+                //check if input is histroy and has no other argumens
+                if(secondInputString.empty()){
                     
-                } else{
-                    //print only that many lines from the historyArray
-                    //TODO set numPrint to the second argument that comes with history
-                    for(int i = historySize; i >= historySize - numPrint; i--){
-                        std::cout << "  " << index << ": " << historyList[i] << "\n";
-                        index = index + 1;
+                    //Delete everything in history
+                } else if(strcmp(secondInput, clear)){
+                    //
+                } else if(isdigit(secondInput[0])){
+                    printf("inside isdigit conditional");
+                    numPrint = std::stoi(input_list[1]);
+                    //If the number is not negative or 0.
+                    if(numPrint > 0){
+                        //print only that many lines from the historyArray
+                        //TODO set numPrint to the second argument that comes with history
+                        
+                        for(int i = historySize - numPrint; i < historySize; i++){
+                            std::cout << "  " << i << ": " << historyList[i] << "\n";
+                        }
                     }
                 }
-                for(int i = 0; i < historySize; i++){
-                    //std::cout << "  " << i << ": " << history_list[i];
-                }        
-        
-            //  For all other commands, check if an executable by that name is in one of the PATH directories
-        
-            //   If no, print error statement: "<command_name>: Error command not found" (do include newline)
+                */    
 
-            } else if(execv(char_os_path_list, command_list_exec) == -1){
+            //   If no, print error statement: "<command_name>: Error command not found" (do include newline)
+            } else if(execv(charInput, command_list_exec) == -1){
                 std::cout << input << ": Error command not found\n"; 
                 continue;
-            }
-            //  If input is a legal command -- execute it
 
-            size++;
+             //  If input is a legal command -- execute it
+            //  For all other commands, check if an executable by that name is in one of the PATH directories
+            } else {
+                printf("inside the execute if statement");
+                void *shmem = createSharedMemory(sizeof(int));
+
+                int pid = fork();
+
+                //child
+                if(pid == 0)
+                {
+                    //execute the first element
+                    bool fileExists;
+                    fileExists = false;
+                    
+                    while(fileExists){
+                        for(int i = 0; i < os_path_list.size(); i++){
+                            string_os_path_list = os_path_list[i];
+                            char * char_os_path_list = new char[string_os_path_list.size() + 1];
+                            std::copy(string_os_path_list.begin(), string_os_path_list.end(), char_os_path_list);
+                            char_os_path_list[string_os_path_list.size()] = '\0'; // don't forget the terminating 0
+
+                            if(execv(charInput, command_list_exec) == -1){
+                                fileExists = true;
+                            }
+                        }
+                    }
+        
+                    if(fileExists == true){
+                        //executeFile
+                        execv(char_os_path_list, command_list_exec);
+                    } else{
+                        std::cout << "ERROR\n";
+                    }
+                    
+                   
+                    //execv(char_os_path_list, command_list_exec);
+                }
+                //parent
+                else
+                {
+                    int status;
+                    waitpid(pid, &status, 0);
+                }
+            }
+
         }
     }
 
@@ -275,6 +342,14 @@ void vectorOfStringsToArrayOfCharArrays(std::vector<std::string>& list, char ***
         strcpy((*result)[i], list[i].c_str());
     }
     (*result)[list.size()] = NULL;
+}
+
+void* createSharedMemory(size_t size)
+{
+    int protection = PROT_READ | PROT_WRITE;
+    int visibility = MAP_SHARED | MAP_ANONYMOUS;
+
+    return mmap(NULL, size, protection, visibility, -1, 0);
 }
 
 /*
